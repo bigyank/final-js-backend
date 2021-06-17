a31531c629e669225c2787c265c879073e553935
bigyank <this.is.blueorange@gmail.com>
2021-06-17 14:02:21 +0545

    more validation

--- src/validators/posts.validator.js
+++ src/validators/posts.validator.js
@@ -5,12 +5,20 @@ export const makePostValidator = {
   [Segments.BODY]: Joi.object().keys({
     title: Joi.string().required(),
     body: Joi.string().required(),
+    image: Joi.string().required(),
+    type: Joi.string().required(),
+    lat: Joi.number().required(),
+    lon: Joi.number().required(),
   }),
 };
 
 export const editPostValidator = {
   [Segments.BODY]: Joi.object().keys({
-    title: Joi.string(),
-    body: Joi.string(),
+   title: Joi.string().required(),
+    body: Joi.string().required(),
+    image: Joi.string().required(),
+    type: Joi.string().required(),
+    lat: Joi.number().required(),
+    lon: Joi.number().required(),
   }),
 };
